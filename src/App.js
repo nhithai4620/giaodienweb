4ea05db83f31a9379143f2e1f6a2a0e0274689d8
thanhlongst2020 <68694926+thanhlongst2020@users.noreply.github.com>
2021-01-19 11:47:30 +0700

    giaodienweb

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div>
+            {/*<h1>AppComponent</h1>*/}
+           <Header></Header>
+    </div>
+  );
+}
+
+export default App;
