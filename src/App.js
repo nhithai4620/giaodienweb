c43ee6001eef9719b3098037a6b842247ccd8ff7
thanhlongst2020 <68694926+thanhlongst2020@users.noreply.github.com>
2021-01-20 16:14:03 +0700

    Cap nhat giao dien

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
+// import theSinhVien from './component/theSinhVien';
+
+import theSinhVien from './component/theSinhVien';
+import giayPhepLaiXe from './component/giayPhepLaiXe';
 
 function App() {
   return (
     <div>
             {/*<h1>AppComponent</h1>*/}
-           <Header></Header>
+           {/* <Header></Header> */}
+           {/* <theSinhVien></theSinhVien> */}
+           <giayPhepLaiXe></giayPhepLaiXe>
     </div>
   );
 }
 
-export default App;
+export default giayPhepLaiXe;
